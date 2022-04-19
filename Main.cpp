f7483dd366bed26e5f293b1e7f8b259e0e0d6efd
Parberry, Ian <Ian.Parberry@unt.edu>
2022-04-19 18:42:53 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -8,30 +8,30 @@
 /// `Input.txt` containing a list of strings, one per line. Blank lines are
 /// ignored. Output goes to a hard-coded file `Output.txt`. The standard 
 /// pseudo-random permutation generation algorithm is used, based on the 
-/// pseudo-random number generator `std::default_random_engine` seeded with the
-/// Unix epoch time in seconds.
+/// pseudo-random number generator `std::default_random_engine` seeded with
+/// the Unix epoch time in seconds.
 
 // MIT License
 //
 // Copyright (c) 2022 Ian Parberry
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+// IN THE SOFTWARE.
 
 #include <iostream>
 #include <fstream>
